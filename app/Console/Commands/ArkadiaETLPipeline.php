b93aae2bcd99406b158119f3d8cb83a1b1ee48e3
mambaitamcuy <adhlirrazak514@gmail.com>
2026-07-03 19:19:25 +0800

    Changed the ArkadiaLp

--- app/Console/Commands/ArkadiaETLPipeline.php
+++ app/Console/Commands/ArkadiaETLPipeline.php
@@ -14,31 +14,35 @@ class ArkadiaETLPipeline extends Command
     {
         $this->info('--- Memulai ETL Pipeline Arkadia ---');
         
+        // Ambil nama database dari env agar penulisan query lebih bersih
+        $dwh = env('DB_DATABASE_DWH') . '.';
+        $oltp = env('DB_DATABASE_OLTP') . '.';
+        
         try {
             // 1. Amankan data dimensi terlebih dahulu secara bertahap
             $this->comment('Sinkronisasi data dimensi (Waktu, Cabang, Produk)...');
             
-            // Isi Dimensi Waktu dummy
-            DB::statement("INSERT IGNORE INTO " . env('DB_DATABASE_DWH') . "dwh_dim_waktu (id_waktu, tanggal, hari, bulan, nama_bulan, kuartal, tahun) 
+            // Isi Dimensi Waktu dummy (Ditambahkan titik setelah nama DB)
+            DB::statement("INSERT IGNORE INTO {$dwh}dwh_dim_waktu (id_waktu, tanggal, hari, bulan, nama_bulan, kuartal, tahun) 
                             VALUES (1, '2026-01-01', 'Kamis', 1, 'Januari', 1, 2026);");
             
-            // Isi Dimensi Cabang dummy
-            DB::statement("INSERT IGNORE INTO " . env('DB_DATABASE_DWH') . "dwh_dim_cabang (id_dim_cabang, nama_cabang) 
+            // Isi Dimensi Cabang dummy (Ditambahkan titik setelah nama DB)
+            DB::statement("INSERT IGNORE INTO {$dwh}dwh_dim_cabang (id_dim_cabang, nama_cabang) 
                             VALUES (1, 'Cabang Utama');");
 
             // Sinkronisasi data master produk langsung dari tabel laptops OLTP
-            DB::statement("INSERT IGNORE INTO " . env('DB_DATABASE_DWH') . "dwh_dim_produk (id_dim_produk, nama_produk)
-                            SELECT id, nama FROM " . env('DB_DATABASE_OLTP') . ".laptops;");
+            DB::statement("INSERT IGNORE INTO {$dwh}dwh_dim_produk (id_dim_produk, nama_produk)
+                            SELECT id, nama FROM {$oltp}laptops;");
 
             // 2. Kosongkan data fakta lama sebelum diisi yang baru
             $this->comment('Mengosongkan tabel dwh_fact_penjualan...');
-            DB::statement('TRUNCATE TABLE ' . env('DB_DATABASE_DWH') . 'dwh_fact_penjualan;');
+            DB::statement("TRUNCATE TABLE {$dwh}dwh_fact_penjualan;");
 
             // 3. Tarik data fakta penjualan dari tabel relasi baru
             $this->comment('Memindahkan data transaksi dari OLTP ke DWH via detail_penjualan...');
             
             DB::statement("
-                INSERT INTO " . env('DB_DATABASE_DWH') . "dwh_fact_penjualan (
+                INSERT INTO {$dwh}dwh_fact_penjualan (
                     id_waktu, id_dim_produk, id_dim_cabang, id_penjualan, metode_pembayaran, 
                     qty, harga_jual, harga_modal, subtotal, profit, created_at
                 )
@@ -54,9 +58,9 @@ class ArkadiaETLPipeline extends Command
                     (l.harga * dp.jumlah) AS subtotal,     -- Kalkulasi subtotal (harga * qty)
                     (l.harga - (l.harga * 0.80)) * dp.jumlah AS profit, -- Kalkulasi profit untung bersih
                     NOW() 
-                FROM arkadialp_oltp.detail_penjualan dp
-                JOIN arkadialp_oltp.laptops l ON dp.id_produk = l.id
-                JOIN arkadialp_oltp.penjualan p ON dp.id_penjualan = p.id_penjualan;
+                FROM {$oltp}detail_penjualan dp
+                JOIN {$oltp}laptops l ON dp.id_produk = l.id
+                JOIN {$oltp}penjualan p ON dp.id_penjualan = p.id_penjualan;
             ");
             
             $this->info('--- ETL Pipeline Berhasil Dijalankan! Data DWH telah sinkron dengan detail_penjualan. ---');
